c0bc5d6ad7907db37679816eb018d7fc77ea2d23
Rahul <rsc120@nist.edu>
2024-04-07 01:46:39 +0530

    UI Fixes

--- frontend/src/Header.js
+++ frontend/src/Header.js
@@ -1,16 +1,44 @@
-import React from 'react'
+import React, { useState,useEffect } from 'react'
 import { Link } from 'react-router-dom'
 const Header = () => {
+  const [userName, setUserName] = useState('');
+  useEffect(() => {
+    fetch('http://localhost:4000/profile', {
+      credentials: 'include'
+    }).then(response => {
+      response.json().then(userInfo => {
+        setUserName(userInfo.userName);
+      })
+    })
+  }, []);
+  function logout(){
+    fetch('http://localhost:4000/logout', {
+      credentials: 'include',
+      method:'POST'
+    }) 
+    setUserName(null);
+  }
   return (
     <header>
-        <div className="logoContainer">
-          <Link to ="/" className="logoName">Interview Voyage</Link>
-        </div>
-        <div className="nav">
-          <Link to ="/login"> Login </Link>
-          <Link to ="/register"> Register </Link>
-        </div>
-      </header>
+      <div className="logoContainer">
+        <Link to="/" className="logoName">Interview Voyage</Link>
+      </div>
+      <div className="nav">
+        {userName && (
+          <>
+            <Link to='/create'>Share Experience</Link>
+            <a onClick={logout}>Logout</a>
+          </>
+        )}
+        {!userName && (
+          <>
+            <Link to="/login"> Login </Link>
+            <Link to="/register"> Register </Link>
+          </>
+        )
+        }
+      </div>
+    </header>
   )
 }
 
