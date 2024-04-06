980eb05a346751d037599e909a3f69ba9596cabd
Rahul <rsc120@nist.edu>
2024-04-07 00:20:55 +0530

    Frontend Updated

--- frontend/src/pages/LoginPage.js
+++ frontend/src/pages/LoginPage.js
@@ -1,11 +1,34 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import {Navigate} from 'react-router-dom'
 const LoginPage = () => {
-  return (
-    <form className='login'>
+  const [userName,setUserName]=useState('');
+  const [password,setPassword]=useState('');
+  const [redirect,setRedirect]=useState(false);
+  async function login(ev){
+    const response=await fetch('http://localhost:4000/login', {
+      method: 'POST',
+      body: JSON.stringify({ userName, password }),
+      headers: { 'Content-type': 'application/json' },
+      credentials:'include'
+    })
+    if(response.status===200)
+      setRedirect(true);
+    else
+      alert("Invalid Credentials");
+    }
+    if(setRedirect)
+      return <Navigate to={'/'} />
+    return (
+    <form className='login' onSubmit={login}>
       <h1>Login</h1>
-      <input type="text" placeholder='Username'/>
-      <input type="password" placeholder='Password' />
+      <input type="text" placeholder='Username'
+      value={userName}
+      onChange={ev=>setUserName(ev.target.value)}
+      />
+      <input type="password" placeholder='Password'
+      value={password}
+      onChange={ev=>setPassword(ev.target.value)}
+      />
       <button>Login</button>
     </form>
   )
--- frontend/src/pages/RegisterPage.js
+++ frontend/src/pages/RegisterPage.js
@@ -1,13 +1,34 @@
-import React from 'react'
-
+import React, { useState } from 'react'
 const RegisterPage = () => {
+  const [userName, setUserName] = useState('');
+  const [password, setPassword] = useState('');
+  async function register(ev) {
+    ev.preventDefault();
+    const response = await fetch('http://localhost:4000/register', {
+      method: 'POST',
+      body: JSON.stringify({ userName, password }),
+      headers: { 'Content-type': 'application/json' }
+
+    })
+    if (response.status === 200)
+      alert("Registration Successfull")
+    else
+      alert("Registration failed! Username already exists.Try with a different Username");
+  }
   return (
-    <form className='register'>
-    <h1>Register</h1>
-    <input type="text" placeholder='Username'/>
-    <input type="password" placeholder='Password' />
-    <button>Register</button>
-  </form>  
+    <form className='register' onSubmit={register}>
+      <h1>Register</h1>
+      <input type="text"
+        placeholder='Username'
+        value={userName}
+        onChange={ev => setUserName(ev.target.value)}
+      />
+      <input type="password" placeholder='Password'
+        value={password}
+        onChange={ev => setPassword(ev.target.value)}
+      />
+      <button>Register</button>
+    </form>
   )
 }
 
