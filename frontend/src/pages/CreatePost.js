168b15f51e21b1cacc098820728987f06d0a4c46
Rahul <rsc120@nist.edu>
2024-04-11 17:27:43 +0530

    Input validation bug fixes

--- frontend/src/pages/CreatePost.js
+++ frontend/src/pages/CreatePost.js
@@ -1,62 +1,65 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
-import {Navigate} from 'react-router-dom'
+import { Navigate } from 'react-router-dom'
 const CreatePost = () => {
-    const [title,setTitle]=useState('');
-    const [summary,setSummary]=useState('');
-    const [content,setContent]=useState('');
-    const [files,setFiles]=useState('');
-    const [redirect,setRedirect]=useState(false);
-    async function createNewPost(ev){
+    const [title, setTitle] = useState('');
+    const [summary, setSummary] = useState('');
+    const [content, setContent] = useState('');
+    const [files, setFiles] = useState('');
+    const [redirect, setRedirect] = useState(false);
+    async function createNewPost(ev) {
         ev.preventDefault();
-        const data=new FormData();
-        data.set('title',title);
-        data.set('summary',summary);
-        data.set('content',content);
-        data.set('file',files[0]);
-        const response=await fetch('http://localhost:4000/post',{
-            method:'POST',
-            body:data,
-            credentials:'include'
+        const data = new FormData();
+        data.set('title', title);
+        data.set('summary', summary);
+        data.set('content', content);
+        data.set('file', files[0]);
+        const response = await fetch('http://localhost:4000/post', {
+            method: 'POST',
+            body: data,
+            credentials: 'include'
         })
-        if(response.ok){
+        if (response.ok) {
             alert("Posted Successfully");
             setRedirect(true);
         }
-        else{
+        else {
             alert("Failed! Please try again later");
         }
     }
-  if(redirect)
-    return <Navigate to={'/'}></Navigate>
-  return (
-   <>
-   <div className="createNewpost">
-        Share your Interview experience and help others ace their preperation 👩🏻‍💻
-   </div>
-   <form onSubmit={createNewPost}>
-        <input type="text" placeholder={'Enter title for exmple: Interview Experience @ Company Name or Interview Experience for @ Position'} 
-        value={title} 
-        onChange={ev=>setTitle(ev.target.value)}
-        />
-        <input type="summary" placeholder={'Give a summary of your experience such as Positive / Negative Experience , CTC offered, On campus/ Off Campus, Selected/ Not Selected , Position appled for etc..'}
-        value={summary}
-        onChange={ev=>setSummary(ev.target.value)}
-        />
-        <input type="file"
-        // value={files}
-        onChange={ev=>setFiles(ev.target.files)}
-        />
-        <ReactQuill className='quill'
-            placeholder={'Upload image/logo of the company you interviewed in or any relevant image in the above space provided. Enter your Interview Experience in details here. Warning : DO NOT INCLUDE ANY CONFIDENTIAL DATA OR INVOLVE IN PIRACY.'}
-            value={content}
-            onChange={newValue=>setContent(newValue)}
-        ></ReactQuill>
-        <button style={{marginTop:'10px', marginBottom:'170px'}}>Share Experience</button>
-   </form>
-   </>
-  )
+    if (redirect)
+        return <Navigate to={'/'}></Navigate>
+    return (
+        <>
+            <div className="createNewpost">
+                Share your Interview experience and help others ace their preperation 👩🏻‍💻
+            </div>
+            <form onSubmit={createNewPost}>
+                <input type="text" placeholder={'Enter title for exmple: Interview Experience @ Company Name or Interview Experience for @ Position'}
+                    value={title}
+                    required={true}
+                    onChange={ev => setTitle(ev.target.value)}
+                />
+                <input type="summary" placeholder={'Give a summary of your experience such as Positive / Negative Experience , CTC offered, On campus/ Off Campus, Selected/ Not Selected , Position appled for etc..'}
+                    value={summary} s
+                    required={true}
+                    onChange={ev => setSummary(ev.target.value)}
+                />
+                <input type="file"
+                    // value={files}
+                    required={true}
+                    onChange={ev => setFiles(ev.target.files)}
+                />
+                <ReactQuill className='quill'
+                    placeholder={'Upload image/logo of the company you interviewed in or any relevant image in the above space provided. Enter your Interview Experience in details here. Warning : DO NOT INCLUDE ANY CONFIDENTIAL DATA OR INVOLVE IN PIRACY.'}
+                    value={content}
+                    onChange={newValue => setContent(newValue)}
+                ></ReactQuill>
+                <button style={{ marginTop: '10px', marginBottom: '170px' }}>Share Experience</button>
+            </form>
+        </>
+    )
 }
 
 export default CreatePost
--- frontend/src/pages/EditPost.js
+++ frontend/src/pages/EditPost.js
@@ -1,38 +1,38 @@
-import React , {useEffect, useState} from 'react'
+import React, { useEffect, useState } from 'react'
 import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
-import {useParams,Navigate} from 'react-router-dom'
+import { useParams, Navigate } from 'react-router-dom'
 const EditPost = () => {
     const [title, setTitle] = useState('');
     const [summary, setSummary] = useState('');
     const [content, setContent] = useState('');
     const [files, setFiles] = useState('');
     const [redirect, setRedirect] = useState(false);
-    const {id}=useParams();
-    useEffect(()=>{
+    const { id } = useParams();
+    useEffect(() => {
         fetch(`http://localhost:4000/post/${id}`)
-            .then(response=>{
-                response.json().then(postInfo=>{
+            .then(response => {
+                response.json().then(postInfo => {
                     setTitle(postInfo.title);
                     setContent(postInfo.content);
                     setSummary(postInfo.summary);
                 })
             })
-    },[])
-    async function updatePost(ev){
+    }, [])
+    async function updatePost(ev) {
         ev.preventDefault();
-        const data=new FormData();
-        data.set('title',title);
-        data.set('summary',summary);
-        data.set('content',content);
-        data.set('id',id);
-        if(files?.[0]){
-            data.set('file',files?.[0]);
+        const data = new FormData();
+        data.set('title', title);
+        data.set('summary', summary);
+        data.set('content', content);
+        data.set('id', id);
+        if (files?.[0]) {
+            data.set('file', files?.[0]);
         }
-        await fetch('http://localhost:4000/post',{
-            method:'PUT',
-            body:data,
-            credentials:'include'
+        await fetch('http://localhost:4000/post', {
+            method: 'PUT',
+            body: data,
+            credentials: 'include'
         });
 
         setRedirect(true);
@@ -47,10 +47,12 @@ const EditPost = () => {
             <form onSubmit={updatePost}>
                 <input type="text" placeholder={'Title'}
                     value={title}
+                    required={true}
                     onChange={ev => setTitle(ev.target.value)}
                 />
                 <input type="summary" placeholder={'Summary'}
                     value={summary}
+                    required={true}
                     onChange={ev => setSummary(ev.target.value)}
                 />
                 <input type="file"
