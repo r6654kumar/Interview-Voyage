cb0ee15068d63b5c9bb5b82837e75d531048de7c
0rigin-c0de <shunnu807@gmail.com>
2024-05-13 01:15:07 +0530

    initial-commit

--- frontend/src/pages/CreatePost.js
+++ frontend/src/pages/CreatePost.js
@@ -15,7 +15,7 @@ const CreatePost = () => {
         data.set('summary', summary);
         data.set('content', content);
         data.set('file', files[0]);
-        const response = await fetch('http://localhost:4000/post', {
+        const response = await fetch('https://interview-voyage-dkbv.onrender.com/post', {
             method: 'POST',
             body: data,
             credentials: 'include'
